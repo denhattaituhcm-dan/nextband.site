7fecf3b862d87d7785027e3baeba9e5d6b2e8868
denhattaituhcm-dan <denhattaituhcm@gmail.com>
2026-08-16 16:51:25 +0700

    k

--- ielts-api/nextband/src/layouts/MinimalLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-export default function MinimalLayout() {
-  return (
-    <div className="min-h-screen bg-background">
-      <Outlet />
-    </div>
-  );
-}
