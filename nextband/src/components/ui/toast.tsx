3611afb9d4378a395e74b99bd7dc91497307078b
denhattaituhcm-dan <284556546+denhattaituhcm-dan@users.noreply.github.com>
2026-08-30 00:27:12 +0700

    Update toast.tsx

--- nextband/src/components/ui/toast.tsx
+++ nextband/src/components/ui/toast.tsx
@@ -9,11 +9,3 @@ export type ToastProps = {
 };
 
 export type ToastActionElement = React.ReactElement<any>;
-
-export const ToastProvider: React.FC<{ children: React.ReactNode; duration?: number }> = ({ children }) => <>{children}</>;
-export const ToastViewport: React.FC<any> = () => null;
-export const Toast: React.FC<any> = () => null;
-export const ToastTitle: React.FC<any> = () => null;
-export const ToastDescription: React.FC<any> = () => null;
-export const ToastClose: React.FC<any> = () => null;
-export const ToastAction: React.FC<any> = () => null;
